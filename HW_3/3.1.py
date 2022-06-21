176f5fdb082c642fe35720b959fb8465aab20153
Vseslav Cringe_ovnik <computerservice.minsk@gmail.com>
2022-06-21 18:02:24 +0300

    rename

---
+++ HW_3/3.1.py
@@ -0,0 +1,9 @@
+# This is homework
+print('1)')
+a = int(input('a? '))
+b = int(input('b? '))
+sum_ = a + b
+diff = a - b
+mult = a * b
+print(f'sum = {sum_} difference = {diff} product = {mult}')
+
